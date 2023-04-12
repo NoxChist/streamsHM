a58ceb9efac8ab2246356aebf1c389b2a700fad1
Kristina Bazaley <atra.nox.ch@gmail.com>
2023-04-13 03:21:19 +0700

    Task2Done

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,51 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> result = new ArrayList<>();
-        for (int num : list) {
-            if (num > 0 && num % 2 == 0) {
-                result.add(num);
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+//Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+        Stream stream1 = persons.stream()
+                .filter(x -> x.getAge() < 18);
+        System.out.println(stream1.count());
+
+//Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+        List<String> inTheArmyNow = persons.stream()
+                .filter(x -> x.getAge() > 17 && x.getAge() < 28)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+//Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием в выборке
+//(т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
+        Comparator<Person> personComparator = (o1, o2) -> {
+            if (o1.getFamily().compareToIgnoreCase(o2.getFamily()) == 0) {
+                if (o1.getAge() - o2.getAge() == 0) {
+                    return o1.getName().compareToIgnoreCase(o2.getName());
+                }
+                return o1.getAge() - o2.getAge();
             }
-        }
-        result.sort(Comparator.naturalOrder());
-        for (int num : result) {
-            System.out.println(num);
-        }
+            return o1.getFamily().compareToIgnoreCase(o2.getFamily());
+        };
+        List<Person> toWork = persons.stream()
+                .filter(x -> x.getAge() > 17 && x.getAge() < 65)
+                .filter(x -> !(x.getAge() > 59 && x.getSex() == Sex.FEMALE))
+                .filter(x -> x.getEducation() == Education.HIGHER)
+                .sorted(personComparator)
+                .collect(Collectors.toList());
+
+
     }
 }
